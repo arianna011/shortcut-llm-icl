012520f8a6fa32c04d9b31f92f08997f78388939
Arianna Paolini <ariannapaolini011@gmail.com>
2025-10-12 15:37:29 +0200

    fix double init in sweep

--- patched_unibias/WB_logging.py
+++ patched_unibias/WB_logging.py
@@ -194,7 +194,10 @@ def log_evaluation_run(
     else:
         run_name += f'_baseline'
 
-    run = wandb.init(project=WB_PROJECT_NAME, name=run_name, job_type="evaluation", config=config, tags=tags or [], 
+    if wandb.run is not None: # running inside a sweep
+        run = wandb.run
+    else: # intialize new standalone run
+        run = wandb.init(project=WB_PROJECT_NAME, name=run_name, job_type="evaluation", config=config, tags=tags or [], 
                      settings = wandb.Settings(init_timeout=240))
 
     if repE_active:
