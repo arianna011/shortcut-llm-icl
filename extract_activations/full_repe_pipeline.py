299c07641fe29b29e02284bad1c65e0e2664fd24
Arianna Paolini <ariannapaolini011@gmail.com>
2025-12-07 10:10:34 +0100

    small fix

--- extract_activations/full_repe_pipeline.py
+++ extract_activations/full_repe_pipeline.py
@@ -395,8 +395,8 @@ def RepE_evaluation(
         mul_shorts = True
         train_dataset_art_names = []
         act_art_names = []
-    elif isinstance(training_dataset_shortcut, str):
-        training_dataset_shortcut = [training_dataset_shortcut]
+    elif isinstance(training_dataset_shortcut_types, str):
+        training_dataset_shortcut_types = [training_dataset_shortcut_types]
     
     for shortcut_type in training_dataset_shortcut_types:
         training_dataset_art_name = L.get_dataset_artifact_name(
